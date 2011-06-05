5758c6bcdf0b87bec271a18273d1890a5f6c81f6
alexey <alexey@alexey-laptop.504-s.auditory.ru>
2011-06-05 16:31:26 +0400

    hail

--- web/js/getProCaptcha.js
+++ web/js/getProCaptcha.js
@@ -1,3 +1,9 @@
+var state = "A";
+var intId = 0;
+var nameRefresh = '.refresh';
+var hash = $("input[name='captcha_hash']");
+var captchaImage = $("img[alt='captcha']");
+
 window.onload = function(){
 //$(document).ready(function() {
 	sendAnswer("http://procaptcha/captcha/getCaptcha");
@@ -10,4 +16,24 @@ function sendAnswer(url){
     success: function(data){
 		$("#procaptcha_container").html(data);
 	}
-});}
+});
+	$(nameRefresh).live("click",function(){
+		clickHandler();
+	});
+}
+function clickHandler(){
+	//alert("It's Alive");
+	refreshCaptcha("http://procaptcha/captcha/getImage");
+}
+function refreshCaptcha(url){
+$.ajax({
+	type: "GET",
+	url: url,
+    success: function(data){
+	//alert(data);
+	//alert($("input[name='captcha_hash']").attr("name"));
+	$("img[alt='captcha']").attr("src","/images/captcha/"+data+".png");
+	$("input[name='captcha_hash']").attr("value",data);
+	}
+});
+}
